f7dd92e8880de4d6bf29e46cdc8ae4de86d7a751
Jiening <Jiening.tao@loop.colum.edu>
2023-12-15 16:19:48 +0800

    Camo Test & interactable test
    use pro builder to fix UV to use Camo
    added 1st version of the interactable system

---
+++ Assets/TestReciver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestReciver : MonoBehaviour
+{
+
+
+
+    public void Trigger()
+    {
+        this.gameObject.SetActive(!gameObject.active);
+    }
+}
